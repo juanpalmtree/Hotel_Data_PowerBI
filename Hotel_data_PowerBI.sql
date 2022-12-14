9dd06bb75ea314a7eb378880e5ad557a046c595d
Juan Palma <110362963+juanpalmtree@users.noreply.github.com>
2022-12-14 14:26:32 -0500

    Update Hotel_data_PowerBI.sql

--- Hotel_data_PowerBI.sql
+++ Hotel_data_PowerBI.sql
@@ -1,33 +1,30 @@
+/*The following script uses the hotels table by combining data from three different tables: dbo['2018$'], dbo['2019$'], and dbo['2020$']. 
+This hotels table is then used to answer the following questions: 
+
+1. Is the hotel revenue growing by year?
+
+To answer this question, the following query can be used:*/
+
+SELECT
+    arrival_date_year,
+    hotel,
+    ROUND(SUM((stays_in_week_nights + stays_in_weekend_nights) * adr), 2) AS Revenue
+FROM hotels
+GROUP BY arrival_date_year, hotel;
+
+/* This query creates a new Revenue column by calculating the sum of the product of the 
+stays_in_week_nights and stays_in_weekend_nights columns with the adr (average daily rate) column. 
+This Revenue column is then used to group the data by year and hotel in order to see if revenue is growing by year.
 
 
-with hotels as (
-select * from dbo.['2018$']
-union
-select * from dbo.['2019$']
-union
-select * from dbo.['2020$'])
+2. How does market segment and meal cost affect revenue?
 
+To answer this question, the following query can be used:*/
 
---is the hotel revenue growing by year?
---there is no "revenue" column, but we can create a new column in order to see revenue
---adr = average daily rate
+SELECT *
+FROM hotels
+LEFT JOIN dbo.market_segment$ ON hotels.market_segment = market_segment$.market_segment
+LEFT JOIN dbo.meal_cost$ ON meal_cost$.meal = hotels.meal;
 
-/* 
-
-select 
-arrival_date_year, hotel,
-round(sum((stays_in_week_nights + stays_in_weekend_nights)*adr),2) as Revenue from hotels
-group by arrival_date_year, hotel;
-
-*/
-
--- this following query allows us to join the remaining 2 tables "market_segment$" and "meal_cost$"
--- after joining this, we are now able to organize the data together and prepare it for visualization 
-
-
-select * from hotels
-left join dbo.market_segment$ 
-on hotels.market_segment = market_segment$.market_segment
-left join dbo.meal_cost$
-on meal_cost$.meal = hotels.meal
-
+/*This query joins the hotels table with the dbo.market_segment$ and dbo.meal_cost$ tables on the market_segment and meal columns, respectively.
+This allows the data to be organized together and prepared for visualization, which can help show how market segment and meal cost affect revenue.*/
